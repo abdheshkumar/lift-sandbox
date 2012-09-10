953bef741d51ab47bd605b1f49fdc9c4f704799a
Tim Nelson <tim@eltimn.com>
2012-09-10 13:33:10 -0500

    Alt version based on suggestion

--- metamapper-trait/src/main/scala/eltimn/model/Inventory.scala
+++ metamapper-trait/src/main/scala/eltimn/model/Inventory.scala
@@ -1,45 +1,36 @@
 package eltimn.model
 
+import net.liftweb.common._
 import net.liftweb.mapper._
+import net.liftweb.util.Helpers
 
-class Inventory extends LongKeyedMapper[Inventory] with ElasticSearchUpdater[Inventory] {
+class Inventory extends ElasticSearchUpdate[Inventory] {
   def getSingleton = Inventory
+}
+
+object Inventory extends Inventory with InventoryUpdater[Inventory] {}
+
+trait ElasticSearchUpdate[T <: ElasticSearchUpdate[T]] extends LongKeyedMapper[T] with Loggable {
+  self: T =>
 
   def primaryKeyField = id
   object id extends MappedLongIndex(this)
 
-  def updateElasticSearch: Unit = getSingleton.updateElasticSearch(this)
-}
-
-object Inventory extends Inventory with LongKeyedMetaMapper[Inventory] with InventoryUpdater[Inventory] {
-
-  def findAllByIdGreaterThan(n: Int): List[Inventory] =
-    findAll(By_>=(id, n))
-}
-
-
-trait InventoryUpdater[T <: Mapper[T]] {
-  self: MetaMapper[T] =>
-
-  def findAllByIdGreaterThan(n: Int): List[T]
-
-  def updateElasticSearch(inst: Mapper[T]): Unit = {
-    ()
+  def updateElasticSearch: Unit = {
+    logger.info("Starting search sync for Inventory id %d" format id.is)
   }
+}
+
+trait InventoryUpdater[T <: ElasticSearchUpdate[T]] extends LongKeyedMetaMapper[T] with Loggable {
+  self: T =>
 
   def updateInventory: Unit = {
     var items = List(create)
     var x = 0
-    //logger.info("Starting search sync for Inventory table at %s" format Helpers.now)
-    while ({items = findAllByIdGreaterThan(x); items.nonEmpty}){
-      items.foreach { row => updateElasticSearch(row) }
+    logger.info("Starting search sync for Inventory table at %s" format Helpers.now)
+    while ({items = findAll(By_>=(id, x)); items.nonEmpty}){
+      items.foreach { row => row.updateElasticSearch }
       x = x + 1000
     }
   }
-}
-
-trait ElasticSearchUpdater[T <: Mapper[T]] {
-  self: Mapper[T] =>
-
-  def updateElasticSearch: Unit
 }
